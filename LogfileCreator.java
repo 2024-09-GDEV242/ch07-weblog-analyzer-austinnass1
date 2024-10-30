d6facaa1a1beef1ef59c5b6103be75ea660d85d4
Austin Nass <austinnass1@outlook.com>
2024-10-30 10:42:27 -0400

    Project6Final

--- LogfileCreator.java
+++ LogfileCreator.java
@@ -4,8 +4,8 @@ import java.util.*;
 /**
  * A class for creating log files of random data.
  * 
- * @author David J. Barnes and Michael Kölling
- * @version    2016.02.29
+ * @author Austin Nass
+ * @version    10.30.2024
  */
 public class LogfileCreator
 {
--- LoglineTokenizer.java
+++ LoglineTokenizer.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
  * Currently, the log file is assumed to contain simply
  * integer date and time information.
  * 
- * @author David J. Barnes and Michael Kolling.
- * @version    2016.02.29
+ * @author Austin Nass
+ * @version    10.30.2024
  */
 public class LoglineTokenizer
 {
